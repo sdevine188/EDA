a6ee39e42126b498ff9f8c1909680f475db0c1bb
Devine <SDevine@doc.gov>
2016-03-29 17:27:31 -0400

    current clean_shiny_data, not sure if fully working

---
+++ helper_scripts/clean_shiny_data.R
@@ -0,0 +1,147 @@
+library(ggmap)
+library(stringr)
+library(dplyr)
+library(lubridate)
+library(taRifx.geo)
+library(readr)
+
+# set working directory where master_data file is saved
+setwd("G:/PNP/Performance Measurement/master_data")
+
+# read in latest master_data
+master_data_filename <- list.files()[str_detect(list.files(), "master_data_20")]
+master_data <- read.csv(master_data_filename, stringsAsFactors = FALSE, colClasses = c("Control." = "character", 
+                                                                                       "Project.No." = "character", "Proj.ZIP" = "character", "Appl..Zip" = "character",
+                                                                                       "Initiatives" = "character"))
+
+md <- master_data
+
+# read in archived shiny data with lat/long info
+setwd("G:/PNP/Performance Measurement/rshinyapp/grants/data")
+
+filename <- list.files()[str_detect(list.files(), "shiny_app_data_20")]
+shiny_app <- read_csv(filename, col_types = list("Control." = col_character()))
+shiny_app2 <- select(shiny_app, Control., address, lat, lon)
+
+## merge existing shiny data with latest master_data 
+
+# run this is you only want to include approved grants 
+# records <- filter(md, Status == "Approved")
+
+records <- md
+records <- left_join(records, shiny_app2, by = "Control.")
+
+# only needed on 20160209 when changing variable name from address to app_address
+# names(records)[which(names(records) == "address")] <- "app_address"
+
+need_app_address <- filter(records, is.na(app_address))
+
+# only needed when cleaning records previously mapped with just bing, but not google
+# need_app_address_google <- filter(records, !is.na(app_address) & is.na(app_lat))
+
+# compile address field to use in geocoding
+need_app_address$app_address <- str_c(need_app_address$Appl.Street.Addr.1, need_app_address$Appl.City.Name, need_app_address$Appl.State.Abbr, need_app_address$Appl..Zip, sep = ", ")
+need_app_address$app_address <- as.character(need_app_address$app_address)
+
+# geocode addresses
+options(BingMapsKey = "QFG8iBCiInAj6ER1ubuD~I5piVwPPZghOvhCJzBP-1g~AicfV1u7mkoKlY53KfatxR67u-NHXCfu1iEA8dBryA8vlUJy3yu3y0u2cZLWf-D4")
+
+for(i in 1:nrow(need_app_address)){
+        coordinates <- tryCatch(geocode(need_app_address$app_address[i], service = "bing"), 
+                                warning = function(w) {
+                                        if(grepl("geocode failed with status ZERO_RESULTS", w)){
+                                                print("this is a warning")
+                                                address <- need_app_address$app_address[i]
+                                                zip <- need_app_address$Appl..Zip[i]
+                                                geocode(zip, service = "bing")
+                                        }
+                                },
+                                error = function(e) {
+                                        if(grepl("replacement has length zero", e)){
+                                                print("this is an error")
+                                                address <- need_app_address$app_address[i]
+                                                zip <- need_app_address$Appl..Zip[i]
+                                                geocode(zip, service = "bing")
+                                        }
+                                } 
+        )
+        coordinates <- unlist(coordinates)
+        print(i)
+        print(coordinates)
+        if(is.null(coordinates)){
+                print("error: coordinates are null")
+                address <- need_app_address$app_address[i]
+                zip <- need_app_address$Appl..Zip[i]
+                coordinates <- geocode(zip, service = "bing")
+                coordinates <- unlist(coordinates)
+                print(coordinates)
+                need_app_address$app_lon[i] <- coordinates[2]
+                need_app_address$app_lat[i] <- coordinates[1]
+        }
+        need_app_address$app_lon[i] <- coordinates[2]
+        need_app_address$app_lat[i] <- coordinates[1]
+}
+
+# see how many apps bing could not map, even when trying just the zip code
+# usually these records have too many or too few zip code digits, but some are OCONUS and bing just can't map them
+# try running them through goolgle just in case
+bing_failed <- need_app_address[which(is.na(need_app_address$app_lat)), ]
+dim(bing_failed)
+need_app_address_google <- bing_failed
+# only need this if cleaning records previously mapped in just bing, not google
+# need_app_address_google <- rbind(need_app_address_google, bing_failed)
+
+for(i in 1:nrow(need_app_address_google)){
+        coordinates <- tryCatch(geocode(need_app_address_google$app_address[i]), 
+                                warning = function(w) {
+                                        if(grepl("geocode failed with status ZERO_RESULTS", w)){
+                                                print("this is a warning")
+                                                address <- need_app_address_google$app_address[i]
+                                                zip <- need_app_address_google$Appl..Zip[i]
+                                                geocode(zip, service = "bing")
+                                        }
+                                },
+                                error = function(e) {
+                                        if(grepl("replacement has length zero", e)){
+                                                print("this is an error")
+                                                address <- need_app_address_google$app_address[i]
+                                                zip <- need_app_address_google$Appl..Zip[i]
+                                                geocode(zip, service = "bing")
+                                        }
+                                } 
+        )
+        coordinates <- unlist(coordinates)
+        print(i)
+        print(coordinates)
+        if(is.null(coordinates)){
+                print("error: coordinates are null")
+                address <- need_app_address_google$app_address[i]
+                zip <- need_app_address_google$Appl..Zip[i]
+                coordinates <- geocode(zip, service = "bing")
+                coordinates <- unlist(coordinates)
+                print(coordinates)
+                need_app_address_google$app_lon[i] <- coordinates[2]
+                need_app_address_google$app_lat[i] <- coordinates[1]
+        }
+        need_app_address_google$app_lon[i] <- coordinates[2]
+        need_app_address_google$app_lat[i] <- coordinates[1]
+}
+
+# see how many of the NA's google was not able to map
+length(which(is.na(need_app_address_google$app_lat)))
+
+# re-combine the existing master_data with the newly mapped applications
+need_app_address_minus <- need_app_address[-which(is.na(need_app_address$app_lat)), ]
+need_app_address_combined <- rbind(need_app_address_minus, need_app_address_google)
+records_minus <- filter(records, !is.na(app_lat)) 
+shiny_app_data <- rbind(records_minus, need_app_address_combined)
+
+# if posting publically, clean shiny_data_app to remove PII
+# need to confirm the fields match eda press releases and usaspending.gov
+
+# write shiny data to file
+date1 <- as.character(Sys.Date())
+date2 <- str_replace_all(date1, "-", "")
+shiny_filename <- str_c("shiny_app_data_", date2, ".csv")
+write_csv(shiny_data_app, path = shiny_filename)
+
