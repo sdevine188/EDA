5ee7867ebf623b1032dbbf5f14637f1b4280a3ef
sdevine188 <sdevine188@gmail.com>
2016-12-05 12:46:21 -0500

    updated function to have keep_columns arg defaulting to control number, to enable better review and post-review joins

--- helper_scripts/find_keyword_matches.R
+++ helper_scripts/find_keyword_matches.R
@@ -1,25 +1,32 @@
 # visually check keyword-only hits
-species_keywords <- iris %>% select(Species)
+dataframe <- iris
 keyword_string <- "setosa|versicolor"
 
-find_keyword_matches <- function(dataframe, keyword_string){
+find_keyword_matches <- function(dataframe, keyword_string, keep_columns = c("Control.No.")){
         keyword_matches <- data.frame(matrix(nrow = nrow(dataframe), ncol = ncol(dataframe)))
         names(keyword_matches) <- names(dataframe)
         for(col in 1:ncol(dataframe)){
                 column_values <- c()
-                for(row in 1:nrow(dataframe)){
-                        if(grepl(keyword_string, dataframe[row, col], ignore.case = TRUE)){
-                                column_values <- c(column_values, as.character(dataframe[row, col]))
-                        } else {
-                                column_values <- c(column_values, NA)
-                        }   
+                if(names(dataframe)[col] %in% keep_columns) {
+                        column_values <- dataframe[ , col]
+                        keyword_matches[ , col] <- column_values
+                        next
                 }
-                keyword_matches[ , col] <- column_values
+                if(!(names(dataframe)[col] %in% keep_columns)) {
+                        for(row in 1:nrow(dataframe)){
+                                if(grepl(keyword_string, dataframe[row, col], ignore.case = TRUE)){
+                                        column_values <- c(column_values, as.character(dataframe[row, col]))
+                                } else {
+                                        column_values <- c(column_values, NA)
+                                }   
+                        }
+                        keyword_matches[ , col] <- column_values
+                }
         }
         return(keyword_matches)
 }
 
-species_keywords_only <- find_keyword_matches(species_keywords, keyword_string)
+species_keywords_only <- find_keyword_matches(dataframe, keyword_string, keep_columns = "Sepal.Length")
 write_csv(species_keywords_only, "species_keywords_only.csv")
 
 
